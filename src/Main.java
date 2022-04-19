e28c31cf5eb2a8506d6abb59fe3b65a1610fbe2e
Magdalena <krystynapawlowicz@wp.pl>
2022-04-19 19:54:03 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String month = "April";
+        int year = 2022;
+        long day = 19;
+        System.out.println("miesiąc:" + month);
+        System.out.println("rok:" + year);
+        System.out.println("dzień:" + day);
+    }
+
+}
